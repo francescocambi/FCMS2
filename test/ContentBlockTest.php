d173c78bd46cbaf2c70ad7bac8d3a9930aa88e54
francescocambi <francesco.cambi@gmail.com>
2014-09-18 13:30:37 +0200

    Test Init

---
+++ test/ContentBlockTest.php
@@ -0,0 +1,40 @@
+<?php
+/** Copyright 2014 Francesco Cambi                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *     http://www.apache.org/licenses/LICENSE-2.0                           *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           */
+/**
+ * User: Francesco
+ * Date: 17/09/14
+ * Time: 15.04
+ */
+
+namespace test;
+
+class ContentBlockTest extends \PHPUnit_Framework_TestCase {
+
+    private $instance = null;
+    /**
+     * @before
+     */
+    public function initialize() {
+        $this->instance = new \ContentBlock(0, "blocco", "blocco", "<h1>Contenuto</h1>", null, "url",0,2,0,0.3,false,true,"contain");
+    }
+
+    public function testGetHTML() {
+
+    }
+
+    public function testGetBackgroundCSS() {
+        $css = $this->instance->getBackgroundCSS();
+        $this->assertEquals("background: url('url') rgba(0,2,0,0.3) repeat-y; background-size: contain;", $css);
+    }
+
+}
+ 
